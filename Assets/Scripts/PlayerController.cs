2894c4e3570138b448e1adefcb42651cc63f2fb8
parkdae12 <subinesl@naver.com>
2024-01-30 17:52:08 +0900

    버튼 이동 추가작업

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private CatEscapeGameDirector gameDirector;
+    [SerializeField] private Button btnLeft;
+    [SerializeField] private Button btnRight;
     public float radius = 1f;
+    public int maxHp = 13;
+    public int hp;
+    private void Awake()
+    {
+        this.hp = this.maxHp;
+        Debug.LogFormat("<color=yellow>{0}/{1}</color>", this.hp, this.maxHp);
+    }
+    private void Start()
+    {
+        this.btnLeft.onClick.AddListener(() => {
+            Debug.Log("왼쪽 화살표 버튼 클릭");
+            this.transform.Translate(-2, 0, 0);
+        });
+        this.btnRight.onClick.AddListener(() =>
+        {
+            Debug.Log("오른쪽 화살표 버튼 클릭");
+            this.transform.Translate(2, 0, 0);
+        });
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -21,4 +44,14 @@ public class PlayerController : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(this.transform.position, this.radius);
     }
+
+    public void LeftButtonClick()
+    {
+        Debug.Log("Left Button Click");
+    }
+    public void RigthButtonClick()
+    {
+        Debug.Log("Rigth Button Click");
+    }
+
 }
