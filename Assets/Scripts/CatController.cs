a46247bd622140a848f6b2c60826e0fdeeaaa82c
parkdae12 <subinesl@naver.com>
2024-01-30 18:56:46 +0900

    필드수정작업

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
-    [SerializeReference] private Rigidbody2D rbody;
+    [SerializeField] private Rigidbody2D rbody;
     [SerializeField] private float force = 680f;
     // Update is called once per frame
     void Update()
